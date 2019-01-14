87abd2b7c725f3752d374ed5107c63dc262c2edb
Yuriko Aya <thechildoflily@gmail.com>
2019-01-14 10:03:30 +0700

    fix dynamic index link

--- per-category-navigation.php
+++ per-category-navigation.php
@@ -31,11 +31,9 @@ function the_nav($content) {
             if($currentCat[1]->cat_name == 'Novel') {
                 $cat_name = $currentCat[0]->name;
                 $cat_id = $currentCat[0]->cat_ID;
-                $cat_link = $currentCat[0]->slug;
             } else {
                 $cat_name = $currentCat[1]->cat_name;
                 $cat_id = $currentCat[1]->cat_ID;
-                $cat_link = $currentCat[1]->slug;
             }
         }
     
@@ -47,6 +45,7 @@ function the_nav($content) {
         );
 
         $cat_lists = get_posts($args);
+        $cat_link = get_category_link($cat_id);
 
         foreach($cat_lists as $category) {
             $post_list[] = $category->post_title;
@@ -64,8 +63,8 @@ function the_nav($content) {
         } else {
             $next = '<div class="col-sm-4 text-right percanav"><a href="/' .$link_list[$location+1].'">NEXT >><br>' .$post_list[$location+1]. '</a></div>';
         }
-        $cat_index = '<div class="col-sm-4 text-center percanav"><a href="/category/novel/' .$cat_link.'">INDEX <br>' .$cat_name. '</a></div>';
-        
+        $cat_index = '<div class="col-sm-4 text-center percanav"><a href="'.$cat_link.'">INDEX <br>' .$cat_name. '</a></div>';
+
         $cat_nav = '<div class="row">'. $prev.$cat_index.$next .'</div>';
 
         return $content.$cat_nav;
